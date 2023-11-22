624973ef417d2f7fbefff40215d85c6331ddd6cb
Oscar <oscar.vasquez@havasmagma.com>
2023-11-22 11:10:19 -0600

    tets

--- 03/vite-project/src/main.js
+++ 03/vite-project/src/main.js
@@ -1,4 +1,6 @@
+import "./../style.css";
 import * as THREE from "three";
+import gsap from "gsap";
 
 // SIZES
 const sizes = {
@@ -17,20 +19,16 @@ const axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper);
 
 // OBJECT
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
-mesh.scale.x = 2;
-mesh.scale.y = 0.5;
-mesh.scale.z = 0.5;
 
-mesh.rotation.x = Math.PI * 0.25;
-mesh.rotation.y = Math.PI * 0.25;
+const cube = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: "red" }));
+scene.add(cube);
 
 // CAMERA
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.heigth);
-camera.position.z = 3;
+camera.position.z = 2;
+camera.position.x = 2;
+camera.position.y = 2;
+camera.lookAt(cube.position);
 scene.add(camera);
 
 // RENDERER
@@ -38,8 +36,16 @@ const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 });
 renderer.setSize(sizes.width, sizes.heigth);
-renderer.render(scene, camera);
 
-setInterval(() => {
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  cube.rotation.y = elapsedTime;
+
   renderer.render(scene, camera);
-}, 1000 / 60);
+  window.requestAnimationFrame(tick);
+};
+
+tick();
